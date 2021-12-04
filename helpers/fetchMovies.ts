215fc79acb98e19509fe5e8ed021ca19def8e658
Maksim Smetanin <maks.s923@mail.ru>
2021-12-04 23:31:18 +0500

    Add helpers wrapper for fetch.She is installing headers

---
+++ helpers/fetchMovies.ts
@@ -0,0 +1,12 @@
+const API_KEY: string | undefined = process.env.API_KEY
+
+export default function fetchMovies(url: string): Promise<Response> | Error {
+  if (!API_KEY) throw new Error('api key not found')
+
+  return fetch(url, {
+    headers: {
+      'accept': 'application/json',
+      'X-API-KEY': API_KEY
+    }
+  })
+}
