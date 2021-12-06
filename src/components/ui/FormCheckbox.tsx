c3c78419da49a28b34e8b96b1456bf483f8aed50
Maksim Smetanin <maks.s923@mail.ru>
2021-12-06 21:59:50 +0500

    Create ui component FormInput and Input Checkbox

---
+++ src/components/ui/FormCheckbox.tsx
@@ -0,0 +1,45 @@
+import React, {FC, InputHTMLAttributes} from 'react';
+import {BsCheck} from "react-icons/bs";
+import {useField} from "formik";
+
+interface FormCheckbox extends InputHTMLAttributes<HTMLInputElement> {
+  label: string,
+  name: string,
+  required?: boolean,
+  className?: string
+}
+
+const FormCheckbox: FC<FormCheckbox> = ({label, required, className, ...props}) => {
+
+  const [field, meta] = useField(props.name)
+
+  return (
+    <div className={`mb-5 relative space-y-1 ${className}`}>
+      <label
+        className="block text-sm text_gray_color cursor-pointer flex items-center"
+        onPointerDown={e => e.preventDefault()}
+      >
+        <input
+          value={field.value}
+          checked={field.checked}
+          onChange={field.onChange}
+          onBlur={field.onBlur}
+          type="checkbox"
+          className="form-checkbox opacity-0 h-0 hidden"
+          {...props}
+        />
+        <span
+          className="border border-gray-300 w-4 h-4 inline-block rounded-sm mr-2 flex items-center justify-center">
+                    <BsCheck className="flex-shrink-0" size={20} color='#fff'/>
+                  </span>
+        <span>Я принимаю условие пользовательского соглашения</span>
+        {required && <span className="text-red-400"> *</span>}
+      </label>
+      {meta.error && meta.touched ?
+        <span className="text-red-400 text-xs absolute">{meta.error}</span> : null
+      }
+    </div>
+  );
+};
+
+export default FormCheckbox;
---
+++ src/components/ui/FormInput.tsx
@@ -0,0 +1,39 @@
+import React, {FC, InputHTMLAttributes} from 'react';
+import {useField} from "formik";
+
+interface IFormInput extends InputHTMLAttributes<HTMLInputElement> {
+  label: string,
+  name: string,
+  type: string,
+  required?: boolean,
+  className?: string
+}
+
+const FormInput: FC<IFormInput> = ({label, required, className, ...props}) => {
+
+  const [field, meta] = useField(props.name)
+
+  return (
+    <div className={`mb-3 relative pb-3 space-y-1 ${className}`}>
+      <label
+        htmlFor={props.id || props.name}
+        className="block font-medium text-lg">
+        {label}
+        {required && <span className="text-red-400"> *</span>}
+      </label>
+      <input
+        className="border border-solid border-gray-400 block w-full py-2 px-2 rounded-md
+        focus:outline-none focus:border-primary-light shadow-md"
+        {...props}
+        value={field.value}
+        onChange={field.onChange}
+        onBlur={field.onBlur}
+      />
+      {meta.error && meta.touched ?
+        <span className="text-red-400 text-xs absolute">{meta.error}</span> : null
+      }
+    </div>
+  );
+};
+
+export default FormInput;
