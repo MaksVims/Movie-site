36bac356b4c78373a97e33ceaf7bb7afc115bd02
Maksim Smetanin <maks.s923@mail.ru>
2021-12-08 21:35:07 +0500

    Adding to login page FormLoader component

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -4,23 +4,21 @@ import Link from 'next/link'
 import LoginForm from "@/components/auth/LoginForm";
 import {LoginFormValues} from "#/validationTypes";
 import FirebaseAuthService from "@/api/FirebaseAuthService";
+import FormLoader from "@/components/ui/FormLoader";
+import {useFetch} from "@/hooks/useFetch";
 
 const Login: NextPage = () => {
-
-  const login = useCallback(async (values: LoginFormValues) => {
+  const [login, loading, error] = useFetch(useCallback(async (values: LoginFormValues) => {
     const {email, password} = values
-    try {
-      await FirebaseAuthService.login(email, password)
-    } catch (e) {
-      alert(e)
-    }
-  }, [])
+    await FirebaseAuthService.login(email, password)
+  }, []))
+
 
   return (
     <div className="bg-black">
       <main className="flex items-center justify-center px-4 py-6 bg-auth bg-no-repeat bg-top">
-        <section className="h mx-auto max-w-2xl w-full bg-white py-6 px-4 rounded-md shadow-md">
-          <div className="space-y-12 sm:space-y-20">
+        <section className="h mx-auto max-w-2xl w-full bg-white rounded-md shadow-md relative">
+          <div className="space-y-12 sm:space-y-20 py-6 px-4">
             <div className="space-y-1 text-center">
               <h1 className="text-2xl text-black font-semibold">Авторизация</h1>
               <div className="flex items-center justify-center space-x-1 text-sm">
@@ -32,6 +30,7 @@ const Login: NextPage = () => {
             </div>
             <LoginForm handlerSubmit={login}/>
           </div>
+          {loading && <FormLoader/>}
         </section>
       </main>
     </div>
