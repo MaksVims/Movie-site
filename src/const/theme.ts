bb2ebf665d5d36cab8421b506b2568c9fec53350
Maksim Smetanin <maks.s923@mail.ru>
2021-12-09 17:13:17 +0500

    Made a separate file for importing themes in js

---
+++ src/const/theme.ts
@@ -0,0 +1,6 @@
+// @ts-ignore
+import resolveConfig from 'tailwindcss/resolveConfig'
+// @ts-ignore
+import tailwindConfig from '/tailwind.config'
+
+export default resolveConfig(tailwindConfig).theme
