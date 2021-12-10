360e4a5194feaef298ea8fd788770e32293c3978
Maksim Smetanin <maks.s923@mail.ru>
2021-12-10 19:28:26 +0500

    Implements logic components FavoriteMovieList.tsx and FavoriteMovieListItem.tsx

--- src/components/profile/FavoriteMovieList.tsx
+++ src/components/profile/FavoriteMovieList.tsx
@@ -1,27 +1,46 @@
-import React, {FC} from 'react';
-import {IMovie} from "#/movieTypes";
+import React, {FC, useCallback} from 'react';
 import FavoriteMovieListItem from "@/components/profile/FavoriteMovieListItem";
+import {TCollectionItem} from "#/storeTypes";
+import FirebaseCollectionService from "@/api/FirebaseCollectionService";
+import {CollectionState} from "@/store";
 
 interface FavoriteMovieListProps {
   title: string,
-  movies: IMovie[],
+  movies: TCollectionItem[],
   classNames?: string
 }
 
 const FavoriteMovieList: FC<FavoriteMovieListProps> = ({title, movies, classNames}) => {
+  const mapRecords = CollectionState.mapRecordsToCollection
+
+  const removeFavoriteMovie = useCallback(async (record: string) => {
+    await FirebaseCollectionService.removeMovieToCollection(record)
+  }, [])
+
   return (
-    <div className={`overflow-x-auto ${classNames}`}>
-      <h2 className="font-semibold text-xl text-lg mb-2">{title}</h2>
-      <ul role="list">
-        {
-          movies.map(movie => (
-            <FavoriteMovieListItem
-              key={movie.kinopoiskId}
-              movie={movie}
-            />
-          ))
-        }
-      </ul>
+    <div className={`overflow-x-auto w-full h-full ${classNames}`}>
+      {
+        movies.length ? (
+          <>
+            <h2 className="text-center font-semibold text-xl text-lg mb-2">{title}</h2>
+            <ul role="list" className=" space-y-3">
+              {
+                movies.map(movie => (
+                  <FavoriteMovieListItem
+                    key={movie.title}
+                    movie={movie}
+                    handle={removeFavoriteMovie.bind(null, mapRecords[movie.movieId])}
+                  />
+                ))
+              }
+            </ul>
+          </>
+        ) : (
+          <div className="full flex-center">
+            <span className="font-medium">Фильмы осутствуют</span>
+          </div>
+        )
+      }
     </div>
   );
 };
--- src/components/profile/FavoriteMovieListItem.tsx
+++ src/components/profile/FavoriteMovieListItem.tsx
@@ -1,14 +1,27 @@
 import React, {FC} from 'react';
-import {IMovie} from "#/movieTypes";
+import {RiCloseFill} from 'react-icons/ri'
+import Link from 'next/link'
+import theme from "@/const/theme";
+import {TCollectionItem} from "#/storeTypes";
 
 interface IFavoriteMovieListItem {
-  movie: IMovie
+  movie: TCollectionItem,
+  handle: () => {}
 }
 
-const FavoriteMovieListItem: FC<IFavoriteMovieListItem> = ({movie}) => {
+const FavoriteMovieListItem: FC<IFavoriteMovieListItem> = ({movie, handle}) => {
+
   return (
-    <li className="text-lg">
-      {movie.nameRu}
+    <li className="text-lg flex justify-between items-center">
+      <Link href={`/movies/${movie.movieId}`}>
+        <a className="hover:underline mr-2">{movie.title}</a>
+      </Link>
+      <RiCloseFill
+        color={theme.colors.red[500]}
+        size={23}
+        className="flex-shrink-0 cursor-pointer hover:bg-red-200 rounded-full transition duration-100"
+        onClick={handle}
+      />
     </li>
   );
 };
