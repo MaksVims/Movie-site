0188eb0ec9174a1881e33fa7c18b79a94c679565
Maksim Smetanin <maks.s923@mail.ru>
2021-12-10 20:56:03 +0500

    Add usetoggle hooks for popup flag

---
+++ src/hooks/useToggle.ts
@@ -0,0 +1,14 @@
+import {useCallback, useState} from "react";
+
+export default function useToggle(defaultValue?: boolean):
+  [boolean, (value?: boolean) => void, () => void] {
+  const [isToggle, setIsToggle] = useState<boolean>(defaultValue || false)
+
+  const set = useCallback((value?: boolean) => {
+    setIsToggle(prev => value ?? !prev)
+  }, [setIsToggle])
+
+  const close = useCallback(() => set(false), [set])
+
+  return [isToggle, set, close]
+}
