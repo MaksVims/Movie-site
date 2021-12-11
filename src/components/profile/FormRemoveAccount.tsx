02ad3147094f1be7a5aa42d94cdcb93911bd1f65
Maksim Smetanin <maks.s923@mail.ru>
2021-12-11 23:18:29 +0500

    Refactor big component PopupRemoveAccount -> FormRemoveAccount

---
+++ src/components/profile/FormRemoveAccount.tsx
@@ -0,0 +1,69 @@
+import React, {FC, useEffect, useRef, useState} from 'react';
+import FirebaseAuthService from "@/api/FirebaseAuthService";
+import {useAuth} from "@/contexts/AuthContext";
+import {useAlert} from '@/contexts/AlertContext';
+import errorsMessage from "@/const/errorsMessage";
+import {AlertType} from "#/alertCtxTypes";
+import {useRouter} from "next/router";
+import successMessage from "@/const/successMessage";
+
+interface FormRemoveAccountProps {
+  onClose: () => void
+}
+
+const FormRemoveAccount: FC<FormRemoveAccountProps> = ({onClose}) => {
+  const focusRef = useRef<HTMLInputElement | null>(null)
+  const [currentEmail, setCurrentEmail] = useState('')
+  const router = useRouter()
+  const {user} = useAuth()
+  const {showAlert} = useAlert()
+
+  const isExact = currentEmail === user?.email
+
+  useEffect(() => {
+    setTimeout(() => focusRef.current?.focus())
+  }, [])
+
+  const handlerRemoveAccount = async (): Promise<void> => {
+    try {
+      await FirebaseAuthService.deleteAccount()
+      showAlert(successMessage.REMOVE_ACCOUNT, AlertType.SUCCESS)
+      await router.push('/')
+    } catch (e) {
+      alert(e)
+      showAlert(errorsMessage.REMOVE_ACCOUNT, AlertType.ERROR)
+    }
+  }
+
+  return (
+    <div>
+      <div className="flex-center flex-wrap space-y-6">
+        <input
+          placeholder="Email"
+          type="text"
+          className="input w-full hover:border-red-400 focus:border-red-400"
+          value={currentEmail}
+          onChange={e => setCurrentEmail(e.target.value)}
+          ref={focusRef}
+        />
+        <div className="space-x-4 flex justify-end">
+          <button
+            className="btn text-black hover:bg-gray-300"
+            onClick={onClose}
+          >
+            Отмена
+          </button>
+          <button
+            disabled={!isExact}
+            className="btn btn-danger"
+            onClick={handlerRemoveAccount}
+          >
+            Удалить
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FormRemoveAccount;
---
+++ src/components/profile/PopupRemoveAccount.tsx
@@ -0,0 +1,25 @@
+import React, {FC} from 'react';
+import MainPopup from "@/components/ui/popup/MainPopup";
+import FormRemoveAccount from "@/components/profile/FormRemoveAccount";
+
+interface RemoveAccountPopupProps {
+  onClose: () => void,
+  isOpened: boolean
+}
+
+const PopupRemoveAccount: FC<RemoveAccountPopupProps> = ({onClose, isOpened}) => {
+  return (
+    <MainPopup
+      title="Удаление аккаунта"
+      onClose={onClose}
+      isOpened={isOpened}
+    >
+      <div className="space-y-4">
+        <p className="text-gray-color">Для подтверждения удаления аккаунта введите свой email</p>
+        <FormRemoveAccount onClose={onClose}/>
+      </div>
+    </MainPopup>
+  );
+};
+
+export default PopupRemoveAccount;
