5598cca86f3072a33fa49fe2a30bddf28b7e7504
Maksim Smetanin <maks.s923@mail.ru>
2021-12-11 23:31:09 +0500

    Update FormInput components, can now set ref for inputs

--- src/components/ui/FormInput.tsx
+++ src/components/ui/FormInput.tsx
@@ -1,38 +1,41 @@
-import React, {FC, InputHTMLAttributes} from 'react';
+import React, {HTMLProps} from 'react';
 import {useField} from "formik";
 
-interface IFormInput extends InputHTMLAttributes<HTMLInputElement> {
-  label: string,
-  name: string,
-  type: string,
-  required?: boolean,
-  className?: string
+// type FormInputProps = HTMLProps<HTMLInputElement>
+
+interface FormInputProps extends HTMLProps<HTMLInputElement> {
+  placeholder?: string
 }
 
-const FormInput: FC<IFormInput> = ({label, required, className, ...props}) => {
+const FormInput = React.forwardRef<HTMLInputElement, FormInputProps>(
+  ({label, required, className, ...props}, ref
+  ) => {
 
-  const [field, meta] = useField(props.name)
+    const [field, meta] = useField(props.name!)
 
-  return (
-    <div className={`mb-3 relative pb-3 space-y-1 ${className}`}>
-      <label
-        htmlFor={props.id || props.name}
-        className="block font-medium text-lg">
-        {label}
-        {required && <span className="text-red-400"> *</span>}
-      </label>
-      <input
-        className="input w-full"
-        {...props}
-        value={field.value}
-        onChange={field.onChange}
-        onBlur={field.onBlur}
-      />
-      {meta.error && meta.touched ?
-        <span className="text-red-400 text-xs absolute">{meta.error}</span> : null
-      }
-    </div>
-  );
-};
+    return (
+      <div className={`mb-3 relative pb-3 space-y-1 ${className}`}>
+        <label
+          htmlFor={props.id || props.name}
+          className="block font-medium text-lg">
+          {label}
+          {required && <span className="text-red-400"> *</span>}
+        </label>
+        <input
+          className="input w-full"
+          ref={ref}
+          {...props}
+          value={field.value}
+          onChange={field.onChange}
+          onBlur={field.onBlur}
+        />
+        {meta.error && meta.touched ?
+          <span className="text-red-400 text-xs absolute">{meta.error}</span> : null
+        }
+      </div>
+    );
+  });
+
+FormInput.displayName = 'FormInput'
 
 export default FormInput;
