3ba404645aaacf094419addbd744b93c973175ca
Maksim Smetanin <maks.s923@mail.ru>
2022-07-20 18:47:00 +0500

    fix: fix 404 page height inner container

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,11 +6,11 @@ const FourOhFour = () => {
 
   return (
     <MainLayout>
-      <div className="bg-white grow-1 flex-center">
+      <div className="bg-white grow-1 flex-center full">
         <div className="flex-center flex-col">
-          <h2 className="text-xl text-red-400 mb">Указанная страница не найдена</h2>
+          <h2 className="text-xl text-red-400 mb-4">Указанная страница не найдена</h2>
           <Link href='/' >
-            <span className="btn-success btn-big">Вернуться назад</span>
+            <span className="btn-success btn-big">Вернуться домой</span>
           </Link>
         </div>
       </div>
